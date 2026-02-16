3ea682f5e9f342af418e87b157e25f8b0a316351
Augusto Morais <kurohimestudios@gmail.com>
2026-02-16 16:33:13 -0700

    fix: change spawn point/rate
    Fix how the herbs are spawn and limited it to 5 (later on it will have adjustable spawn rate). Still need to improve how the herbs are "destroyed".

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -3,11 +3,27 @@ extends Node
 
 var herb_scene = preload("res://scenes/herb_scene.tscn")
 
-func _process(delta: float) -> void:
+#this will be changed later
+@export var herbs_per_wave : int = 5
+
+func _ready() -> void:
+	spawn_herb()
+
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_right"):
-		spawn_herb()
+		kill_herb()
 
 func spawn_herb ():
-	var herb = herb_scene.instantiate()
-	$"../Herbs".add_child(herb)
-	herb.position = $"../HerbSpawner".get_children().pick_random().position
+	var available_spots = $"../HerbSpawner".get_children().duplicate()
+	available_spots.shuffle()
+	
+	for h in herbs_per_wave:
+		var herb = herb_scene.instantiate()
+		$"../Herbs".add_child(herb)
+		herb.position = available_spots[h].position
+
+func kill_herb():
+	if $"../Herbs".get_children():
+		$"../Herbs".get_children().pick_random().queue_free()
+	else:
+		spawn_herb()
