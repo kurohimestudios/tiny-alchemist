cf734dd428fa75aa7d5bea28afddb15986e9a1e9
Augusto Morais <kurohimestudios@gmail.com>
2026-02-16 15:47:16 -0700

    feat: add spawn herb functionality
    Now the herbs can be spawn by pressing the right arrow, just a workaround functionality to test the code

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,1 +1,13 @@
+class_name GameManager
 extends Node
+
+var herb_scene = preload("res://scenes/herb_scene.tscn")
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_right"):
+		spawn_herb()
+
+func spawn_herb ():
+	var herb = herb_scene.instantiate()
+	$"../Herbs".add_child(herb)
+	herb.position = $"../HerbSpawner".get_children().pick_random().position
