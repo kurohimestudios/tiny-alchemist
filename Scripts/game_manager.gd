50ecf0edefe50b9f1adfb0e1f969c58cbc986d53
Augusto Morais <kurohimestudios@gmail.com>
2026-02-16 18:17:07 -0700

    feat: kill herb by clicking

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,29 +1,29 @@
 class_name GameManager
+
 extends Node
 
 var herb_scene = preload("res://scenes/herb_scene.tscn")
 
 #this will be changed later
 @export var herbs_per_wave : int = 5
+var alive_herbs : int = 0
 
 func _ready() -> void:
 	spawn_herb()
-
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_right"):
-		kill_herb()
 
 func spawn_herb ():
 	var available_spots = $"../HerbSpawner".get_children().duplicate()
 	available_spots.shuffle()
 	
+	alive_herbs = herbs_per_wave
+	
 	for h in herbs_per_wave:
 		var herb = herb_scene.instantiate()
 		$"../Herbs".add_child(herb)
 		herb.position = available_spots[h].position
+		herb.herb_died.connect(kill_herb)
 
 func kill_herb():
-	if $"../Herbs".get_children():
-		$"../Herbs".get_children().pick_random().queue_free()
-	else:
+	alive_herbs -= 1
+	if alive_herbs == 0:
 		spawn_herb()
---
+++ Scripts/herb.gd
@@ -0,0 +1,9 @@
+extends StaticBody2D
+
+signal herb_died
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			herb_died.emit()
+			queue_free()
