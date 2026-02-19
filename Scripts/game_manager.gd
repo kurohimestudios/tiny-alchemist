14a9e11ab0211becc62710844258e1fd43c00716
Augusto Morais <kurohimestudios@gmail.com>
2026-02-18 18:48:34 -0700

    feat: add probability for herb spawn

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -5,10 +5,12 @@ extends Node
 @onready var background_image : Sprite2D = $"../BushesBackground"
 
 var herb_scene = preload("res://scenes/herb_scene.tscn")
+var background_texture = preload("res://assets/background/garden_background.jpg")
 var current_frame : int = 0
 var kill_count : int = 0
 var current_level : int = 1
-var required_kills : int = 10
+var required_kills : int = 1
+var herb_probalities : Array = [100.0, 0.0, 0.0]
 
 #this will be changed later
 @export var herbs_per_wave : int = 5
@@ -27,10 +29,10 @@ func spawn_herb ():
 		change_level()
 	
 	alive_herbs = herbs_per_wave
-	
 	for h in herbs_per_wave:
 		var herb = herb_scene.instantiate()
 		$"../Herbs".add_child(herb)
+		get_random_herb()
 		herb.setup(current_frame)
 		herb.set_herb_health(current_frame)
 		herb.position = available_spots[h].position
@@ -51,10 +53,35 @@ func set_brightness ():
 #change the level, changing the herb spawn, sprite, and also the background
 func change_level ():
 	current_level += 1
-	required_kills = current_level * 1.25
+	#required_kills = current_level * 1.25
 	kill_count = 0
+	change_probability(current_level)
+
+#get a herb depending on the probability for that, only possible for 3 different herb/level
+func get_random_herb ():
+	var total_weight = 0.0
+	var index = 0
 	
-	if current_level >= 5:
-		current_frame += 1
-		background_image.texture = load("res://assets/background/garden_background.jpg")
+	for prob in herb_probalities:
+		total_weight += prob
 	
+	var roll = randf_range(0, total_weight)
+	var cursor = 0.0
+	print(roll)
+	
+	for p in herb_probalities:
+		print(p)
+		cursor += p
+		if roll <= cursor:
+			current_frame = index
+			break
+		index += 1
+
+#change the probalility depending in which level the player is.
+func change_probability (level : int):
+	if level % 2 == 0:
+		herb_probalities [0] -= 2.5
+		herb_probalities [1] += 2.5
+	if level % 3 == 0:
+		herb_probalities [0] -= 2.5
+		herb_probalities [2] += 2.5
