d780ae73b80fd9d16d52842c902231726f2b26ba
Augusto Morais <kurohimestudios@gmail.com>
2026-02-18 19:39:37 -0700

    feat: implement swap probabilities
    Introduce a new function that will change the probabilities to be able to introduce new herbs, it will constantly change it when the requirements are met

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -4,17 +4,20 @@ extends Node
 
 @onready var background_image : Sprite2D = $"../BushesBackground"
 
+#this will be changed later
+@export var herbs_per_wave : int = 5
+
 var herb_scene = preload("res://scenes/herb_scene.tscn")
 var background_texture = preload("res://assets/background/garden_background.jpg")
-var current_frame : int = 0
+
+var current_frames : Array = [0, 1, 2]
 var kill_count : int = 0
 var current_level : int = 1
 var required_kills : int = 1 #temporary value
 var herb_probalities : Array = [100.0, 0.0, 0.0]
-
-#this will be changed later
-@export var herbs_per_wave : int = 5
 var alive_herbs : int = 0
+var base_probability : float = 2.5
+var prob_upgrade : float = 1.0 #will be used when upgrades be launched
 
 func _ready() -> void:
 	set_brightness()
@@ -32,9 +35,9 @@ func spawn_herb ():
 	for h in herbs_per_wave:
 		var herb = herb_scene.instantiate()
 		$"../Herbs".add_child(herb)
-		get_random_herb()
-		herb.setup(current_frame)
-		herb.set_herb_health(current_frame)
+		var herb_index = get_random_herb()
+		herb.setup(herb_index)
+		herb.set_herb_health(herb_index)
 		herb.position = available_spots[h].position
 		herb.herb_died.connect(kill_herb)
 	
@@ -68,25 +71,34 @@ func get_random_herb ():
 	var roll = randf_range(0, total_weight)
 	var cursor = 0.0
 	
+	#need to normalize it so it will be always 100.0
 	for p in herb_probalities:
 		cursor += p
 		if roll <= cursor:
-			current_frame = index
-			break
+			return current_frames[index]
 		index += 1
 
 #change the probalility depending in which level the player is.
 func change_probability (level : int):
 	if herb_probalities [0] < herb_probalities [2]:
-		swap_herbs()
+		swap_probabilities()
 	
 	if level % 2 == 0:
-		herb_probalities [0] -= 2.5
-		herb_probalities [1] += 2.5
-	if level % 3 == 0:
-		herb_probalities [0] -= 2.5
-		herb_probalities [2] += 2.5
+		herb_probalities [0] -= base_probability * prob_upgrade
+		herb_probalities [1] += base_probability * prob_upgrade
 
-#future function that will change the herb_probability to initiate a new plant
-func swap_herbs ():
-	pass
+	if level % 3 == 0:
+		herb_probalities [0] -= base_probability * prob_upgrade
+		herb_probalities [2] += base_probability * prob_upgrade
+
+#change the herb_probability and update the current_frames for new plants
+func swap_probabilities ():
+	var index = 0
+	
+	herb_probalities [0] = herb_probalities [1] + herb_probalities [2]
+	herb_probalities [1] = 100 - herb_probalities [0]
+	herb_probalities [2] = 0
+	
+	while index < 3:
+		current_frames [index] += 1
+		index += 1
