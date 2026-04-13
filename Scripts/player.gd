c3400283d9dea37d61dcb9ea4de58d9db0deaf62
Augusto Morais <kurohimestudios@gmail.com>
2026-04-13 17:55:20 -0600

    fix: player's sprite vector

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,5 +6,5 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	anim.current_animation = "mouse_idle"
 
-func _process(delta):
-	global_position = get_global_mouse_position() + Vector2(0,60)
+func _process(_delta):
+	global_position = get_global_mouse_position() + Vector2(0,0)
